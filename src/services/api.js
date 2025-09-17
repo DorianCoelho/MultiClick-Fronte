0bcb9186cf560a2f530032098a11009bf408b095
Dorian Brites <d.brites@sucasuc.com>
2025-09-17 23:17:52 +0200

    fix: actualizar la URL base de la API para usar HTTPS en lugar de HTTP

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { useAuthStore } from '@/stores/auth'
 import router from '@/router'
 
 const api = axios.create({
-  baseURL: "http://beta-omip-httpapi.sucapp.com/api"
+  baseURL: "https://beta-omip-httpapi.sucapp.com/api"
 })
 
 // Adjunta token
